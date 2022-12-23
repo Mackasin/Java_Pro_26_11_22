ee7fcaf53ecc2d35297fa8e9345345b91183a11e
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2022-12-23 18:26:05 +0200

    hm7

---
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm6/ArrayDataException.java
@@ -0,0 +1,19 @@
+package com.hillel.dzhurynskyi.homework.hm6;
+
+public class ArrayDataException extends Exception {
+    public ArrayDataException() {
+        super("Cannot convert character to number");
+    }
+
+    public ArrayDataException(String message) {
+        super("Cannot convert character [ " + message + " ] to number");
+    }
+
+    public ArrayDataException(int i, String message) {
+        super("Cannot convert character [ " + message + " ] to number  of index number [ " + i + " ]");
+    }
+
+    public ArrayDataException(int i, int j, String message) {
+        super("Cannot convert character [ " + message + " ] to number  of index number [" + i + " ] [ " + j + " ]");
+    }
+}
