236583b210987c7d473c51f93bd8c64931ec9181
Dzhurynskyi Maksym <maxsdzh52@gmail.com>
2023-01-18 19:49:37 +0200

    hm13

---
+++ src/main/java/com/hillel/dzhurynskyi/homework/hm13/Main.java
@@ -0,0 +1,9 @@
+package com.hillel.dzhurynskyi.homework.hm13;
+
+public class Main {
+    public static void main(String[] args) {
+        ValueCalculator valueCalculator = new ValueCalculator();
+            valueCalculator.doSomethingWithMultithreading();
+            valueCalculator.doSomethingWithoutMultithreading();
+    }
+}
